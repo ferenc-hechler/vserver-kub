103cffa76468c84ca3de6ccfb1ce9291384d56bc
ferenc-hechler <github@fh.anderemails.de>
2023-06-25 00:35:35 +0200

    add istio script.

--- setup-scripts/04-istio.sh
+++ setup-scripts/04-istio.sh
@@ -11,3 +11,11 @@ helm install istio-ingress istio/gateway -n istio-ingress --set labels.app=istio
 
 # fake lb IP address
 kubectl patch service -n istio-ingress istio-ingress --subresource=status --type="json" --patch "[{ \"op\": \"replace\", \"path\": \"/status/loadBalancer\", \"value\": {\"ingress\": [{\"ip\":\"207.180.253.250\"}]} }]"
+
+# forward default ports to ingress (TODO: add to crontab for restart)
+export HTTP_NODEPORT=$(kubectl -n  istio-ingress get service  istio-ingress -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export HTTPS_NODEPORT=$(kubectl -n  istio-ingress get service  istio-ingress -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export PUBLIC_IP=$(curl ident.me)
+sudo nohup socat TCP-LISTEN:80,fork TCP4:$PUBLIC_IP:$HTTP_NODEPORT  >/dev/null 2>&1 &
+sudo nohup socat TCP-LISTEN:443,fork TCP4:$PUBLIC_IP:$HTTPS_NODEPORT  >/dev/null 2>&1 &
+
